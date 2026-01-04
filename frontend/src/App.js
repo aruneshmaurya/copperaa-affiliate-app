b54a96e474ca53f657ec92c07cbcd137a6b0f1a4
Copperaa Dev <dev@copperaa.com>
2026-01-05 02:50:55 +0530

    Fix: App.js corrupted routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Affiliates from './pages/Affiliates';
+import Commissions from './pages/Commissions';
+import AdminLayout from './components/AdminLayout';
+import AdminDashboard from './pages/AdminDashboard';
+import ProtectedRoute from './components/ProtectedRoute';
+import AffiliateLogin from './pages/AffiliateLogin';
+import AffiliateDashboard from './pages/AffiliateDashboard';
 import AffiliateLayout from './components/AffiliateLayout';
 import AffiliateSettings from './pages/AffiliateSettings';
+import Register from './pages/Register';
 
-// ...
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login-affiliate" element={<AffiliateLogin />} />
+                <Route path="/register" element={<Register />} />
 
-{/* Affiliate Routes */ }
-<Route element={<ProtectedRoute role="affiliate" />}>
-    <Route path="/affiliate" element={<AffiliateLayout />}>
-        <Route path="dashboard" element={<AffiliateDashboard />} />
-        <Route path="settings" element={<AffiliateSettings />} />
-        <Route index element={<Navigate to="dashboard" />} />
-    </Route>
-</Route>
+                {/* Affiliate Routes */}
+                <Route element={<ProtectedRoute role="affiliate" />}>
+                    <Route path="/affiliate" element={<AffiliateLayout />}>
+                        <Route path="dashboard" element={<AffiliateDashboard />} />
+                        <Route path="settings" element={<AffiliateSettings />} />
+                        <Route index element={<Navigate to="dashboard" />} />
+                    </Route>
+                </Route>
 
-{/* Admin Routes */ }
+                {/* Admin Routes */}
                 <Route element={<ProtectedRoute role="admin" />}>
                     <Route path="/admin" element={<AdminLayout />}>
                         <Route path="dashboard" element={<AdminDashboard />} />
@@ -23,8 +38,8 @@ import AffiliateSettings from './pages/AffiliateSettings';
                 </Route>
 
                 <Route path="*" element={<Navigate to="/login-affiliate" />} />
-            </Routes >
-        </Router >
+            </Routes>
+        </Router>
     );
 }
 
